f8cc61071aaf9fb9004333e7ee669c97ec7b5b40
Kadeem140 <Beckford.kadeem@yahoo.com>
2022-03-23 20:31:12 -0400

    Added router function to handle link default behavior and location changes

--- js/index.js
+++ js/index.js
@@ -1,13 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
     //once the content loads i want the event listeners to add to their respective elements
-    document.querySelector('#seePokemon').addEventListener("click", console.log("Hooked up and ready to go G!"))
+    document.querySelector('#seePokemon')
+        .addEventListener("click", console.log("Hooked up and ready to go G!"))
 })
-
-
 
 
 //fetch functions
 function fetchApi(){
     return fetch(' https://pokeapi.co/api/v2/pokemon')
     .then(res => res.json())
+    .then(pokemon => console.log(pokemon))
 }
---
+++ router/router.js
@@ -0,0 +1,9 @@
+const route = (event) => { //function to handle link default behavior
+//handles location changes
+    event = event || window.event; //if no event is given, default event is window.event
+    event.preventDefault(); //prevents default behovior ie. refreshing pages
+    window.history.pushState( {}, "", event.target.href); //uses History API by calling pushState & pushing the anchor's href value.
+    //updates browser URL
+}
+
+window.route = route; //Global access to route function
